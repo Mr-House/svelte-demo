baa3d20f02422b3eaba99a0598631b277c9c47b5
liutao <vaceliu@gmail.com>
2021-08-24 00:55:33 +0800

    add data table

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,62 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import DataTable, {Head, Body, Row, Cell} from '@smui/data-table'
+  import Textfield from '@smui/textfield'
+  import Button, { Label} from '@smui/button'
+  import Checkbox from '@smui/checkbox'
+
+  let list = [
+    {
+      itemName: '染色',
+      range: '70-120',
+      value: '80'
+    }
+  ]
+  let selected = []
+  function addItem() {
+    list = [...list, {
+      itemName: '',
+      range: '',
+      value: ''
+    }]
+  }
+  function delItem() {
+    list = list.filter(el => !selected.includes(el.itemName))
+  }
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
-</main>
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
-</style>
+<div>
+  <Button on:click={addItem} variant="outlined">
+    <Label>添加检验项</Label>
+  </Button>
+  <Button on:click={delItem} variant="outlined">
+    <Label>删除检验项</Label>
+  </Button>
+</div>
+<DataTable style="max-width:100%;">
+  <Head>
+    <Row>
+      <Cell checkbox><Checkbox /></Cell>
+      <Cell>检验项</Cell>
+      <Cell>合理值范围</Cell>
+      <Cell>测量值</Cell>
+    </Row>
+  </Head>
+  <Body>
+    {#each list as item}
+    <Row>
+      <Cell checkbox>
+        <Checkbox bind:group={selected} value={item.itemName}/>
+      </Cell>
+      <Cell>
+        <Textfield bind:value={item.itemName} required/>
+      </Cell>
+      <Cell>
+        <Textfield bind:value={item.range} required/>
+      </Cell>
+      <Cell>
+        <Textfield bind:value={item.value} required/>
+      </Cell>
+    </Row>
+    {/each}
+  </Body>
+</DataTable>
